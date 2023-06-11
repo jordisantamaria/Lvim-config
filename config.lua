bb6b71c66139e261b70609b499de8f0f1d5a0d92
Jordi Santamaria Portoles <jordisantamaria1a@gmail.com>
2023-06-11 18:53:23 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,3 +1,11 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+--
+--
+
+
 --[[
 lvim is the global options object
 Linters should be
@@ -8,6 +16,7 @@ an executable
 -- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
 
 -- general
+
 lvim.log.level = "warn"
 lvim.format_on_save = false
 lvim.colorscheme = "lunarvim"
@@ -18,11 +27,11 @@ lvim.leader = "space"
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 
-lvim.database = {save_location = '~/.config/nvcode_db', auto_execute = 1}
+lvim.database = { save_location = '~/.config/nvcode_db', auto_execute = 1 }
 
 -- MY CONFIG
-vim.g.goyo_width=120
-vim.g.goyo_height=95
+vim.g.goyo_width = 120
+vim.g.goyo_height = 95
 
 
 vim.cmd([[
@@ -55,7 +64,7 @@ vim.cmd([[
     autocmd VimLeavePre * call CleanupStuff()
 ]])
 
-vim.g.qs_highlight_on_keys = {'f', 'F', 't', 'T'}
+vim.g.qs_highlight_on_keys = { 'f', 'F', 't', 'T' }
 
 -- For format code to snippet format
 vim.cmd([[
@@ -75,28 +84,8 @@ vim.g.csv_nomap_l = 1
 
 
 
--- unmap a default keymapping
--- lvim.keys.normal_mode["<C-Up>"] = ""
--- edit a default keymapping
--- lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
 
--- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
--- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
--- local _, actions = pcall(require, "telescope.actions")
--- lvim.builtin.telescope.defaults.mappings = {
---   -- for input mode
---   i = {
---     ["<C-j>"] = actions.move_selection_next,
---     ["<C-k>"] = actions.move_selection_previous,
---     ["<C-n>"] = actions.cycle_history_next,
---     ["<C-p>"] = actions.cycle_history_prev,
---   },
---   -- for normal mode
---   n = {
---     ["<C-j>"] = actions.move_selection_next,
---     ["<C-k>"] = actions.move_selection_previous,
---   },
--- }
+
 
 -- Use which-key to add extra bindings with the leader-key prefix
 lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
@@ -117,92 +106,92 @@ lvim.builtin.which_key.mappings["a"] = {
 }
 
 lvim.builtin.which_key.mappings["b"] = {
-        name = "+Buffer",
-        ["1"] = {":BufferGoto 1<CR>", "Buffer 1"},
-        ["2"] = {":BufferGoto 2<CR>", "Buffer 2"},
-        ["3"] = {":BufferGoto 3<CR>", "Buffer 3"},
-        ["4"] = {":BufferGoto 4<CR>", "Buffer 4"},
-        ["5"] = {":BufferGoto 5<CR>", "Buffer 5"},
-        ["6"] = {":BufferGoto 6<CR>", "Buffer 6"},
-        ["7"] = {":BufferGoto 7<CR>", "Buffer 7"},
-        ["8"] = {":BufferGoto 8<CR>", "Buffer 8"},
-        ["9"] = {":BufferGoto 9<CR>", "Buffer 9"},
-        d = {":bd<CR>", "delete-buffer"},
-        o = {":Bdelete hidden<CR>", "Delete all but current"},
-        a = {":Bdelete all<CR>", "Delete all buffers"},
-        h = {":TSBufToggle highlight<CR>", "Toogle treesitter highlight"},
+  name = "+Buffer",
+  ["1"] = { ":BufferGoto 1<CR>", "Buffer 1" },
+  ["2"] = { ":BufferGoto 2<CR>", "Buffer 2" },
+  ["3"] = { ":BufferGoto 3<CR>", "Buffer 3" },
+  ["4"] = { ":BufferGoto 4<CR>", "Buffer 4" },
+  ["5"] = { ":BufferGoto 5<CR>", "Buffer 5" },
+  ["6"] = { ":BufferGoto 6<CR>", "Buffer 6" },
+  ["7"] = { ":BufferGoto 7<CR>", "Buffer 7" },
+  ["8"] = { ":BufferGoto 8<CR>", "Buffer 8" },
+  ["9"] = { ":BufferGoto 9<CR>", "Buffer 9" },
+  d = { ":bd<CR>", "delete-buffer" },
+  o = { ":Bdelete hidden<CR>", "Delete all but current" },
+  a = { ":Bdelete all<CR>", "Delete all buffers" },
+  h = { ":TSBufToggle highlight<CR>", "Toogle treesitter highlight" },
 }
 
 lvim.builtin.which_key.mappings["d"] = {
-        name = "+Database",
-        t = {":DBUIToggle<CR>", "db ui toogle"},
-        f = {":DBUIFindBuffer<CR>", "db find buffer"},
-        r = {":DBUIRenameBuffer<CR>", "db rename buffer"},
-        l = {":DBUILastQueryInfo<CR>", "db info last query"},
+  name = "+Database",
+  t = { ":DBUIToggle<CR>", "db ui toogle" },
+  f = { ":DBUIFindBuffer<CR>", "db find buffer" },
+  r = { ":DBUIRenameBuffer<CR>", "db rename buffer" },
+  l = { ":DBUILastQueryInfo<CR>", "db info last query" },
 }
 
 lvim.builtin.which_key.mappings["g"] = {
-        name = "+Git",
-        b = {":Gblame<CR>", "blame"},
-        B = {":GBrowse<CR>", "browser current file"},
-        d = {":Gdiffsplit<CR>", "diff split"},
-        D = {":Git diff<CR>", "diff"},
-		p = {"<cmd>PreviewHunk<cr>", "Preview Hunk"},
-        r = {"<cmd>ResetHunk<cr>", "Reset Hunk"},
-        R = {":ResetBuffer<CR>", "Reset buffer"},
-        s = {":Gstatus<CR>", "status"},
-        m = {"<Plug>(git-messenger)", "message"},
-        ["1"] = {":diffget //2<CR>", "Diffget left"},
-        ["3"] = {":diffget //3<CR>", "Diffget right"},
-        o = {":!sh ~/scripts/repo.sh<CR>", "Open repo"},
+  name = "+Git",
+  b = { ":Gblame<CR>", "blame" },
+  B = { ":GBrowse<CR>", "browser current file" },
+  d = { ":Gdiffsplit<CR>", "diff split" },
+  D = { ":Git diff<CR>", "diff" },
+  p = { "<cmd>PreviewHunk<cr>", "Preview Hunk" },
+  r = { "<cmd>ResetHunk<cr>", "Reset Hunk" },
+  R = { ":ResetBuffer<CR>", "Reset buffer" },
+  s = { ":Gstatus<CR>", "status" },
+  m = { "<Plug>(git-messenger)", "message" },
+  ["1"] = { ":diffget //2<CR>", "Diffget left" },
+  ["3"] = { ":diffget //3<CR>", "Diffget right" },
+  o = { ":!sh ~/scripts/repo.sh<CR>", "Open repo" },
 }
 
 lvim.builtin.which_key.mappings["q"] = {
-        name = "+Quickfix",
-        n = {":cnext<CR>"        , "cnext"},
-        p = {":cprev<CR>"        , "cprev"},
-        o = {":copen<CR>"        , "copen"},
-        q = {":cclose<CR>"       , "cclose"},
-        l = {":clast<CR>"        , "clast"},
-        w = {":Rg<CR>"           , "find cursor word"},
-        g = {":Glog %<CR>"       , "File git history"},
-        t = {":TroubleToggle<CR>"       , "TroubleToggle errors"},
+  name = "+Quickfix",
+  n = { ":cnext<CR>", "cnext" },
+  p = { ":cprev<CR>", "cprev" },
+  o = { ":copen<CR>", "copen" },
+  q = { ":cclose<CR>", "cclose" },
+  l = { ":clast<CR>", "clast" },
+  w = { ":Rg<CR>", "find cursor word" },
+  g = { ":Glog %<CR>", "File git history" },
+  t = { ":TroubleToggle<CR>", "TroubleToggle errors" },
 }
 
 lvim.builtin.which_key.mappings["t"] = {
-        name = "+Terminal",
-        [";"] = {":FloatermNew --wintype=normal --height=8<CR>", "Terminal"},
-        n = {":FloatermNew node<CR>", "Node"},
-        f = {":FloatermNew python<CR>", "Python"},
-        t = {":FloatermToggle<CR>", "Python"},
+  name = "+Terminal",
+  [";"] = { ":FloatermNew --wintype=normal --height=8<CR>", "Terminal" },
+  n = { ":FloatermNew node<CR>", "Node" },
+  f = { ":FloatermNew python<CR>", "Python" },
+  t = { ":FloatermToggle<CR>", "Python" },
 }
 
 lvim.builtin.which_key.mappings["x"] = {
-        name = "+Csv",
-        h = {":HiColumn<CR>", "Highlight column"},
-        H = {":HeaderToogle<CR>", "HeaderToogle"},
-        d = {":DeleteColumn<CR>", "Delete column"},
-        D = {":Duplicate<CR>", "Check duplicates in column"},
-        w = {":WhatColumn!<CR>", "Column name"},
-        W = {":WhatColumn<CR>", "Column number"},
-        n = {":NrColumns<CR>", "Num columns"},
-        s = {":SearchInColumn", "Search in column n, /t/"},
+  name = "+Csv",
+  h = { ":HiColumn<CR>", "Highlight column" },
+  H = { ":HeaderToogle<CR>", "HeaderToogle" },
+  d = { ":DeleteColumn<CR>", "Delete column" },
+  D = { ":Duplicate<CR>", "Check duplicates in column" },
+  w = { ":WhatColumn!<CR>", "Column name" },
+  W = { ":WhatColumn<CR>", "Column number" },
+  n = { ":NrColumns<CR>", "Num columns" },
+  s = { ":SearchInColumn", "Search in column n, /t/" },
 }
 
 lvim.builtin.which_key.mappings["c"] = {
-        name = "+Cheatsheets",
-        c = {":e ~/projects/cheatsheets/cleanCode/index.md<CR>", "Clean code"},
-        r = {":e ~/projects/cheatsheets/resources/index.md<CR>", "Resources"},
-        d = {":e ~/projects/cheatsheets/docker/index.md<cr>", "Docker"},
-        p = {":e ~/projects/cheatsheets/python/index.md<cr>", "Python"},
-        v = {":e ~/projects/cheatsheets/vue/index.md<cr>", "Vue"},
-        s = {":e ~/projects/cheatsheets/ddd/index.md<cr>", "Software structure"},
-        a = {":e ~/projects/cheatsheets/aws/index.md<cr>", "Aws"},
-        t = {":e ~/projects/cheatsheets/bash/tmux.md<cr>", "Tmux"},
-        b = {":e ~/projects/cheatsheets/bash/index.md<cr>", "Bash"},
-        m = {":e ~/projects/cheatsheets/markdown/README.md<cr>", "Markdown"},
-        n = {":e ~/projects/cheatsheets/bash/vim/mappings.md<cr>", "Nvim"},
-        q = {":e ~/projects/cheatsheets/postgres/index.md<cr>", "postgresql"},
+  name = "+Cheatsheets",
+  c = { ":e ~/projects/cheatsheets/cleanCode/index.md<CR>", "Clean code" },
+  r = { ":e ~/projects/cheatsheets/resources/index.md<CR>", "Resources" },
+  d = { ":e ~/projects/cheatsheets/docker/index.md<cr>", "Docker" },
+  p = { ":e ~/projects/cheatsheets/python/index.md<cr>", "Python" },
+  v = { ":e ~/projects/cheatsheets/vue/index.md<cr>", "Vue" },
+  s = { ":e ~/projects/cheatsheets/ddd/index.md<cr>", "Software structure" },
+  a = { ":e ~/projects/cheatsheets/aws/index.md<cr>", "Aws" },
+  t = { ":e ~/projects/cheatsheets/bash/tmux.md<cr>", "Tmux" },
+  b = { ":e ~/projects/cheatsheets/bash/index.md<cr>", "Bash" },
+  m = { ":e ~/projects/cheatsheets/markdown/README.md<cr>", "Markdown" },
+  n = { ":e ~/projects/cheatsheets/bash/vim/mappings.md<cr>", "Nvim" },
+  q = { ":e ~/projects/cheatsheets/postgres/index.md<cr>", "postgresql" },
 }
 
 vim.cmd([[
@@ -213,37 +202,37 @@ vim.cmd([[
 ]])
 
 -- esc with jk
-vim.api.nvim_set_keymap('i', 'jk', '<esc>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('i', 'jk', '<esc>', { noremap = true, silent = true })
 
 -- better indenting
-vim.api.nvim_set_keymap('v', '<', '<gv', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('v', '>', '>gv', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<', '<<', {noremap = true, silent = true, nowait = true})
-vim.api.nvim_set_keymap('n', '>', '>>', {noremap = true, silent = true, nowait = true})
+vim.api.nvim_set_keymap('v', '<', '<gv', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '>', '>gv', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<', '<<', { noremap = true, silent = true, nowait = true })
+vim.api.nvim_set_keymap('n', '>', '>>', { noremap = true, silent = true, nowait = true })
 vim.api.nvim_set_keymap('n', '=', '=ii', {})
 
 -- Move line start line end
-vim.api.nvim_set_keymap('', 'H', '^', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('', 'L', '$', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('', 'H', '^', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('', 'L', '$', { noremap = true, silent = true })
 
 -- Search word in buffer
-vim.api.nvim_set_keymap('n', 's', ':HopWord<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', 'S', ':HopChar1<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', 's', ':HopWord<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'S', ':HopChar1<CR>', { noremap = true, silent = true })
 
 -- Tab switch buffer
-vim.api.nvim_set_keymap('n', '<TAB>', ':bnext<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<S-TAB>', ':bprevious<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<TAB>', ':bnext<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<S-TAB>', ':bprevious<CR>', { noremap = true, silent = true })
 
 -- go to ending of the copied/yank text
-vim.api.nvim_set_keymap('v', 'y', 'y`]', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('v', 'p', 'p`]', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', 'p', 'p`]', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', 'y', 'y`]', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 'p', 'p`]', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'p', 'p`]', { noremap = true, silent = true })
 
 -- Move by quickfix faster
-vim.api.nvim_set_keymap('n', '<C-m>', ':cnext<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<C-m>', ':cnext<CR>', { noremap = true, silent = true })
 
 -- Last insertion mark
-vim.api.nvim_set_keymap('n', 'g.', '`^', {silent = true})
+vim.api.nvim_set_keymap('n', 'g.', '`^', { silent = true })
 
 
 
@@ -276,125 +265,50 @@ lvim.builtin.treesitter.highlight.enabled = true
 
 vim.cmd("nnoremap <silent> <C-p> <cmd>lua vim.diagnostic.goto_prev()<cr>")
 vim.cmd("nnoremap <silent> <C-n> <cmd>lua vim.diagnostic.goto_next()<cr>")
--- generic LSP settings
 
--- ---@usage disable automatic installation of servers
--- lvim.lsp.automatic_servers_installation = false
 
--- ---@usage Select which servers should be configured manually. Requires `:LvimCacheRest` to take effect.
--- See the full default list `:lua print(vim.inspect(lvim.lsp.override))`
--- vim.list_extend(lvim.lsp.override, { "pyright" })
-
--- ---@usage setup a server -- see: https://www.lunarvim.org/languages/#overriding-the-default-configuration
--- local opts = {} -- check the lspconfig documentation for a list of all possible options
--- require("lvim.lsp.manager").setup("pylsp", opts)
-
--- you can set a custom on_attach function that will be used for all the language servers
--- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
--- lvim.lsp.on_attach_callback = function(client, bufnr)
---   local function buf_set_option(...)
---     vim.api.nvim_buf_set_option(bufnr, ...)
---   end
---   --Enable completion triggered by <c-x><c-o>
---   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
--- end
--- you can overwrite the null_ls setup table (useful for setting the root_dir function)
--- lvim.lsp.null_ls.setup = {
---   root_dir = require("lspconfig").util.root_pattern("Makefile", ".git", "node_modules"),
--- }
--- or if you need something more advanced
--- lvim.lsp.null_ls.setup.root_dir = function(fname)
---   if vim.bo.filetype == "javascript" then
---     return require("lspconfig/util").root_pattern("Makefile", ".git", "node_modules")(fname)
---       or require("lspconfig/util").path.dirname(fname)
---   elseif vim.bo.filetype == "php" then
---     return require("lspconfig/util").root_pattern("Makefile", ".git", "composer.json")(fname) or vim.fn.getcwd()
---   else
---     return require("lspconfig/util").root_pattern("Makefile", ".git")(fname) or require("lspconfig/util").path.dirname(fname)
---   end
--- end
-
--- -- set a formatter, this will override the language server formatting capabilities (if it exists)
- local formatters = require "lvim.lsp.null-ls.formatters"
- formatters.setup {
-   { exe = "black" },
-   {
-     exe = "prettier",
-     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-     filetypes = { "typescript", "typescriptreact" },
-   },
- }
-
--- -- set additional linters
--- local linters = require "lvim.lsp.null-ls.linters"
--- linters.setup {
---   { exe = "black" },
---   {
---     exe = "eslint_d",
---     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
---     filetypes = { "javascript", "javascriptreact" },
---   },
--- }
 
 -- Additional Plugins
 lvim.plugins = {
-    {"tpope/vim-fugitive"},
-    {"tpope/vim-rhubarb"},
-    {"rhysd/git-messenger.vim"},
-    {"unblevable/quick-scope"}, -- Navigation with f or t
-    {"kana/vim-textobj-user"}, -- common utils for textobj plugins
-    {"kana/vim-textobj-entire"}, -- Object for file ae, file no empty lines ie
-    {"kana/vim-textobj-lastpat"}, -- Object for search, i/ a/
-    {"kana/vim-textobj-indent"}, -- For select text on same indent level with ai
-    {"kana/vim-textobj-line"}, -- For select text on same indent level with ai
-    {"Julian/vim-textobj-variable-segment"}, -- Object for select a variable segment, so snake case or camel case segment, with iv or av
-    {"bkad/CamelCaseMotion"}, -- Motion for camelcase snakecase part
-    {"jordisantamaria/vim-visual-star-search"}, -- Search visual text selected with *
-    {"tpope/vim-abolish"}, -- For better abrev and change snake_case to camelCase with crs, crc
-    {"jordisantamaria/vim-workspace"}, -- Auto save buffer and session
-    {"tpope/vim-surround"},
-    {"tpope/vim-repeat"},
-    {"chrisbra/csv.vim"},
-    {"jordisantamaria/nvcode-color-schemes.vim"},
-    {"christoomey/vim-tmux-navigator"},
-    {"jremmen/vim-ripgrep"},
-    {"junegunn/fzf"},
-    {"airblade/vim-rooter"},
-    {"voldikss/vim-floaterm"},
-    {"Pocco81/TrueZen.nvim"},
-    {"junegunn/vim-easy-align"},
-    {"mzlogin/vim-markdown-toc"},
-    {"dhruvasagar/vim-table-mode"},
-    {"tpope/vim-sleuth"},
-    {"Asheq/close-buffers.vim"},
-    {"tpope/vim-dadbod"},
-    {"kristijanhusak/vim-dadbod-ui"},
-    {"kristijanhusak/vim-dadbod-completion"},
-    {"jordisantamaria/vim-replace"},
-    {"iamcco/markdown-preview.nvim"},
-    {"numToStr/FTerm.nvim"},
-    {"monaqa/dial.nvim"},
-    {"nacro90/numb.nvim"},
-    {"phaazon/hop.nvim"},
-    {"NvChad/nvim-colorizer.lua"},
-    {"windwp/nvim-spectre"},
-    {"simrat39/symbols-outline.nvim"},
-    {"nvim-treesitter/playground"},
-    {"kevinhwang91/nvim-bqf"}, -- better quickfix
-    {"glepnir/lspsaga.nvim"},
-    {"terrortylor/nvim-comment"},
-    {"jordisantamaria/nvim-snippets"},
-    {"pantharshit00/vim-prisma"},
-    {"windwp/nvim-ts-autotag"},
+  { "jordisantamaria/vim-workspace" }, -- Auto save buffer and session
+  { "tpope/vim-fugitive" },            -- :Git for see changes
+  { "tpope/vim-rhubarb" },             -- :GBrowse to open current file in github
+  { "rhysd/git-messenger.vim" },
+  { "unblevable/quick-scope" },        -- Navigation with f or t with colors
+  { "tpope/vim-abolish" },             -- For better abrev and change snake_case to camelCase with crs, crc
+  { "tpope/vim-surround" },            -- to change surrond indicators like "" or ''
+  { "tpope/vim-repeat" },
+  { "chrisbra/csv.vim" },
+  { "jordisantamaria/nvcode-color-schemes.vim" },
+  { "christoomey/vim-tmux-navigator" },
+  { "jremmen/vim-ripgrep" }, -- :Rg command for quicklist
+  { "junegunn/fzf" },
+  { "dhruvasagar/vim-table-mode" },
+  -- {"tpope/vim-sleuth"},
+  -- {"tpope/vim-dadbod"},
+  --   {"kristijanhusak/vim-dadbod-ui"},
+  -- {"kristijanhusak/vim-dadbod-completion"},
+  { "Asheq/close-buffers.vim" }, -- space b o command to close other buffers
+  { "jordisantamaria/vim-replace" },
+  -- {"numToStr/FTerm.nvim"}, -- floating terminal
+  { "phaazon/hop.nvim" }, -- jump with s
+  {"NvChad/nvim-colorizer.lua"}, -- to see hexadecimals like #fff to its color
+      {"nvim-treesitter/playground"},
+      {"kevinhwang91/nvim-bqf"}, -- better quickfix
+  -- {"glepnir/lspsaga.nvim"}, -- no need anymore I think
+  {"pantharshit00/vim-prisma"}, -- prisma syntax suport
+      {"windwp/nvim-ts-autotag"},
+
+
+
+
+
+
 }
 
-require('hop').setup({create_hl_autocmd = true})
+require('hop').setup()
 require 'colorizer'.setup()
 require('nvim-ts-autotag').setup()
 
 vim.api.nvim_set_keymap('n', 's', ":HopChar2<cr>", {silent = true})
 vim.api.nvim_set_keymap('n', 'S', ":HopWord<cr>", {silent = true})
--- Autocommands (https://neovim.io/doc/user/autocmd.html)
--- lvim.autocommands.custom_groups = {
---   { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
--- }
